17b9c069747ce0a13c9ebee5550823a356e16530
Tasha McGlashan <tmcglashan77@gmail.com>
2025-06-20 15:20:40 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './Home.css'; // For styling later
 import { Link } from 'react-router-dom'; 
 import Hero from '../components/Hero.jsx';
-
+import Filter from '../components/Filter.jsx';
 
 
 export default function Home({ addToCart, user }) {
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState ('all');
+  
 
   useEffect(() => {
     async function loadProducts() {
@@ -23,31 +25,45 @@ export default function Home({ addToCart, user }) {
     loadProducts();
   }, []);
 
+  const categories = [ 'all', ...new Set (products.map(p => p.category)) ];
+  const filteredProducts = filter === 'all'
+  ? products: products.filter(p => p.category === filter);
+
   console.log ("Current user:", user);
 
   return (
     <div className="home-page">
     <Hero />
       <h2>New Arrivals</h2>
+      {/* Filter component to select product categories */}
+      <Filter 
+      options = {categories}
+      value= {filter}
+      onChange={setFilter}
+      />
+
+      {/* Display filtered products */}
+
       <div className="product-grid">
-        {products.length === 0 ? (
-          <p>Loading products...</p>
-        ) : (
-          products.map((product) => (
-            <div key={product.id} className="product-card">
-              <Link to={`/product/${product.id}`}>
-              <img 
-                src={product.thumbnail} 
-                alt={product.title}
-                />
-              <h3>{product.title}</h3>
-              </Link>
-              <p>${product.price.toFixed(2)}</p>
-              <button onClick={() => addToCart(product)}>Add to Cart</button>
-            </div>
-          ))
-        )}
+  {filteredProducts.length === 0 ? (
+    <p>Loading products...</p>
+  ) : (
+    filteredProducts.map((product) => (
+      <div key={product.id} className="product-card">
+        <Link to={`/product/${product.id}`}>
+          <img 
+            src={product.thumbnail} 
+            alt={product.title}
+          />
+          <h3>{product.title}</h3>
+        </Link>
+        <p>${product.price.toFixed(2)}</p>
+        <button onClick={() => addToCart(product)}>Add to Cart</button>
       </div>
+    ))
+  )}
+</div>
+
     </div>
   );
 }
